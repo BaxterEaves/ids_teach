1d3397b224f00f34adc2aef29048c31408ed7771
Baxter Eaves <baxyjr@gmail.com>
2015-01-27 20:29:47 -0500

    Cleared away junk

--- ids_teach/cppext/niw_module.pyx
+++ ids_teach/cppext/niw_module.pyx
@@ -1,5 +1,6 @@
 from libcpp.vector cimport vector
 
+
 cdef extern from "niw_mmml.hpp":
     double lgniwmmml(vector[vector[double]] X,
                      vector[vector[double]] lambda_0,
@@ -13,33 +14,10 @@ cdef extern from "niw_mmml.hpp":
                      size_t do_n_calculations)
 
 
-cdef vector[double] conv_to_double_vec(a):
-    cdef vector[double] ret
-    for val in enumerate(a):
-        tmp = val
-        ret.push_back(tmp)
-    return ret
-
-
-cdef vector[vector[double]] np_array_to_double_vec2(a):
-    cdef vector[vector[double]] ret
-    for i in range(a.shape[0]):
-        ret.push_back(conv_to_double_vec(a[i,:]))
-    return ret
-
-
-cdef vector[size_t] conv_to_size_t_vec(a):
-    cdef vector[size_t] ret
-    for val in enumerate(a):
-        tmp = val
-        ret.push_back(tmp)
-    return ret
-
-
 def niw_mmml_mp(args):
     return niw_mmml(*args)
+
 
 def niw_mmml(X, lambda_0, mu_0, kappa_0, nu_0, crp_alpha, Z_start, k_start, hist_start, ncalc):
     val = lgniwmmml(X, lambda_0, mu_0, kappa_0, nu_0, crp_alpha, Z_start, k_start, hist_start, ncalc)
     return val
-
